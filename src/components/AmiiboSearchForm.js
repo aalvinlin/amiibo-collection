bc3fe98cf070c5a185345e50a0a8772e445cd6a0
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2021-08-31 23:57:08 -0700

    create AmiiboSearchForm component

---
+++ src/components/AmiiboSearchForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AmiiboSearchForm = () => (
+  <div className="searchForm">
+      <form name="amiiboSearchForm">
+
+      </form>
+  </div>
+);
+
+export default AmiiboSearchForm;
